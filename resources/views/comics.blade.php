6f33ad211773ffe1cce3a88ce50a29701d2b08e5
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-23 16:50:29 +0200

    Header

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Sono comics</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,51 @@
+<header>
+
+    <div class="bluLine">
+
+        <div>
+
+            <p>dc power visa</p>
+            <p>additional dc sites</p>
+
+        </div>
+
+    </div>
+
+    <div class="header">
+
+        {{-- Logo --}}
+        <div>
+            <a href="#"><img src="../images/dc-logo.png" alt="" width="80px"></a>
+        </div>
+
+        {{-- Menu --}}
+        <div>
+
+            <ul>
+
+                <li><a href=""><div class="p-yb-30">characters</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">comics</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">movies</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">tv</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">games</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">collectibilies</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">videos</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">fans</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">news</div> </a><div class="bar"></div> </li>
+                <li><a href=""><div class="p-yb-30">shop</div> </a><div class="bar"></div> </li>
+
+            </ul>
+
+        </div>
+
+        {{-- Search --}}
+        <div>
+            <input type="text" placeholder="search" width="100px">
+        </div>
+
+    </div>
+
+</header>
+
+
+
