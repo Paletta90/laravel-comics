668ac2d23c30e118ae37b1ce9ea23feeb1263739
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-24 09:47:21 +0200

    Utilizzo config per riprendere i dati dei fumetti

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- Link file.css --}}
     <link rel="stylesheet" href=" {{asset('css/app.css')}} ">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 </head>
 
 <body>
